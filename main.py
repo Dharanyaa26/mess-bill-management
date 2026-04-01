0da761639ea174d148a467804483ba1a7342c1a5
Dharanyaa26 <dharanyaa2003@gmail.com>
2026-04-01 13:30:26 +0530

    Removed database password for security

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ conn = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     user="root",
-    password="Dharan@prathi",
+    password="your_password",
     database="mess_management",
     use_pure=True
 )
